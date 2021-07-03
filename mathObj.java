4415bc49cc7782ac19d0e37391c3bebada03645f
poojapsuku <poojapsuku@gmail.com>
2021-07-03 19:44:57 +0530

    MathObjectCommit

---
+++ mathObj.java
@@ -0,0 +1,21 @@
+public class mathObj {
+     public static void main(String[] args) {
+
+        int a = 100;
+        int b = 50;
+         System.out.println("Maximum = " + Math.max(100, 200));
+         System.out.println("Minimum = " + Math.min(100, 200));
+         System.out.println("Square Root of "+ a +"=" + Math.sqrt(a));
+         System.out.println(Math.pow(a, b));
+         System.out.println("Absolute value   = " + Math.abs(-10.5));
+
+         System.out .println(Math.random()); // to print random numbers
+         
+         System.out .println((int)(Math.random()*100)); // To print a  random number between 0 to 100
+
+        
+    }
+        
+    
+    
+}
