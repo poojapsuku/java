f2c639be4f57589f4a818675058e4e3060471f20
poojapsuku <poojapsuku@gmail.com>
2021-07-03 19:13:59 +0530

    first commit

---
+++ Stringl.java
@@ -0,0 +1,12 @@
+public class Stringl {
+    public static void main(String[] args) {
+        String str = "Welcome Java!";
+        String s = "Java is a object oriented programming language";
+        System.out.println("Length = " + str.length());
+        System.out.println("Upper Case = " + str.toUpperCase()); 
+        System.out.println("Lowe Case= " + str.toLowerCase());
+        System.out.println("Concate = "+ str.concat("\t"+s));
+   
+       }
+       
+   }
